08772f9a2e8179bafb1e2909b2fb7c741ea3a5f6
William Simiao <williamsimiao@gmail.com>
2019-06-18 11:27:21 -0300

    fragments with state conserved & removed files from navigation drawer

--- app/src/main/java/com/example/hsmassistantandroid/ui/activities/SecondActivity.kt
+++ app/src/main/java/com/example/hsmassistantandroid/ui/activities/SecondActivity.kt
@@ -15,13 +15,26 @@ import com.example.hsmassistantandroid.ui.fragments.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 private val TAG: String = SecondActivity::class.java.simpleName
-lateinit var toolbar: ActionBar
 
+enum class TabBarFragmentsNames {
+    USUARIO,
+    CERTIFICADOS,
+    RELACAO,
+    GESTAO,
+}
 
 class SecondActivity : AppCompatActivity(),
     ObjetosListFragment.OnFragmentInteractionListener,
     PainelFragment.OnFragmentInteractionListener,
     gestaoUsuarioFragment.OnFragmentInteractionListener {
+
+    val mFragmentMap = mutableMapOf<TabBarFragmentsNames, Fragment?>()
+    lateinit var activeFragment: Fragment
+
+    val fragmentUsuario = ObjetosListFragment.newInstance()
+    val fragmentGestao = gestaoUsuarioFragment.newInstance()
+    val fragmentRelacao = relacaoFragment.newInstance()
+    val fragmentCertificados = ObjetosListFragment.newInstance()
 
     override fun onFragmentInteraction(uri: Uri) {
         Log.e("AQUI", "Houve interacao ")
@@ -35,46 +48,44 @@ class SecondActivity : AppCompatActivity(),
 
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
-        if (savedInstanceState == null) {
-            val fragment = ObjetosListFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
-                .commit()
-        }
+        supportFragmentManager.beginTransaction().add(R.id.container, fragmentUsuario, "4").hide(fragmentUsuario).commit()
+        supportFragmentManager.beginTransaction().add(R.id.container, fragmentGestao, "3").hide(fragmentGestao).commit()
+        supportFragmentManager.beginTransaction().add(R.id.container, fragmentRelacao, "2").hide(fragmentRelacao).commit()
+        supportFragmentManager.beginTransaction().add(R.id.container, fragmentCertificados, "1").commit()
+        activeFragment = fragmentCertificados
     }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_objetos -> {
                 toolbar.title = "Certificados"
-                val fragment = ObjetosListFragment.newInstance()
-                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
-                    .commit()
+                supportFragmentManager.beginTransaction().hide(activeFragment).show(fragmentCertificados).commit()
+                activeFragment = fragmentCertificados
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_relacao -> {
                 toolbar.title = "Relacoes"
-                val fragment = relacaoFragment.newInstance()
-                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
-                    .commit()
+                supportFragmentManager.beginTransaction().hide(activeFragment).show(fragmentRelacao).commit()
+                activeFragment = fragmentRelacao
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_gestao -> {
                 toolbar.title = "Gestão"
-                val fragment = gestaoUsuarioFragment.newInstance()
-                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
-                    .commit()
+                supportFragmentManager.beginTransaction().hide(activeFragment).show(fragmentGestao).commit()
+                activeFragment = fragmentGestao
+//                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName()).commit()
                 return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_usuario -> {
+                toolbar.title = "Usuário"
+                supportFragmentManager.beginTransaction().hide(activeFragment).show(fragmentUsuario).commit()
+                activeFragment = fragmentUsuario
             }
         }
         false
     }
 
-    private fun openFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
+
 
 //    fun setUpBottomAppBar() {
 //        setSupportActionBar(bottomAppBar)
@@ -86,31 +97,22 @@ class SecondActivity : AppCompatActivity(),
 //    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.bottomappbar_menu, menu)
+//        menuInflater.inflate(R.menu.bottomappbar_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.app_bar_user -> {
-
-                val userOptionsFragment = UserOptionsBottomSheetFragment()
-                userOptionsFragment.show(supportFragmentManager, userOptionsFragment.tag)
-
-                Log.d(TAG, "Click on user")
-            }
-
-            else -> Log.d(TAG, "Estranho isso, não existe outro")
-        }
+//        when (item.itemId) {
+//            R.id.app_bar_user -> {
+//
+//                val userOptionsFragment = UserOptionsBottomSheetFragment()
+//                userOptionsFragment.show(supportFragmentManager, userOptionsFragment.tag)
+//
+//                Log.d(TAG, "Click on user")
+//            }
+//
+//            else -> Log.d(TAG, "Estranho isso, não existe outro")
+//        }
         return super.onOptionsItemSelected(item)
-    }
-
-    //ACTIONS
-//    override fun onSupportNavigateUp() =
-//        findNavController(this, R.id.navHostFragment).navigateUp()
-
-    fun didTapNavigationButton() {
-        val bottomNavDrawerFragment = BottomNavigationDrawerFragment()
-        bottomNavDrawerFragment.show(supportFragmentManager, bottomNavDrawerFragment.tag)
     }
 }
