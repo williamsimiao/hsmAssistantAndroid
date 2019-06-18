f65d6bf4623792572eade8b631fbee006adb3adb
William Simiao <williamsimiao@gmail.com>
2019-06-18 13:36:44 -0300

    add options menu for Trustee fragment only

--- app/src/main/java/com/example/hsmassistantandroid/ui/fragments/TrustFragment.kt
+++ app/src/main/java/com/example/hsmassistantandroid/ui/fragments/TrustFragment.kt
@@ -1,15 +1,22 @@
 package com.example.hsmassistantandroid.ui.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
 import com.example.hsmassistantandroid.R
 import kotlinx.android.synthetic.main.trust_fragment.*
 
+private val TAG: String = TrustFragment::class.java.simpleName
+
 class TrustFragment: Fragment() {
     var isTrustees: Boolean? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         return inflater.inflate(R.layout.trust_fragment, container, false)
@@ -24,4 +31,29 @@ class TrustFragment: Fragment() {
             myTextView.text = "Quem confia em mim"
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        if(isTrustees!!) {
+            inflater.inflate(R.menu.gestao, menu)
+            super.onCreateOptionsMenu(menu, inflater)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.options_add_user -> onOptionAddUserClick()
+            R.id.options_reload -> onOptionReloadClick()
+            else -> Log.d(TAG, "Estranho isso, não existe outro")
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun onOptionReloadClick() {
+
+    }
+
+    fun onOptionAddUserClick() {
+
+    }
+
 }
--- app/src/main/java/com/example/hsmassistantandroid/ui/fragments/relacaoFragment.kt
+++ app/src/main/java/com/example/hsmassistantandroid/ui/fragments/relacaoFragment.kt
@@ -1,13 +1,17 @@
 package com.example.hsmassistantandroid.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.hsmassistantandroid.R
 import com.example.hsmassistantandroid.ui.adapters.myPagerAdapter
 import kotlinx.android.synthetic.main.relacao_fragment.*
+
+private val TAG: String = relacaoFragment::class.java.simpleName
 
 class relacaoFragment : Fragment() {
     override fun onCreateView(
@@ -21,17 +25,8 @@ class relacaoFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-//        val fragmentAdapter = myPagerAdapter(childFragmentManager)
-//        relacaoViewpager.se
-//
-//        tabs_main.setupWithViewPager(viewpager_main)
-
-        initViews()
 
         setupViewPager()
-    }
-
-    private fun initViews() {
     }
 
     private fun setupViewPager() {
@@ -50,8 +45,10 @@ class relacaoFragment : Fragment() {
         relacaoViewpager.adapter = adapter
 
         relacaoTabs.setupWithViewPager(relacaoViewpager)
-
     }
+
+
+
 
     companion object {
         fun newInstance(): relacaoFragment = relacaoFragment()
