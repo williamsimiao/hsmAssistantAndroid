f40e2734f75a86a3481601b79727c99add3105f2
William Simiao <williamsimiao@gmail.com>
2019-07-23 17:32:45 -0300

    added isFirstTime flag

--- app/src/main/java/com/example/hsmassistantandroid/ui/activities/WellcomeActivity.kt
+++ app/src/main/java/com/example/hsmassistantandroid/ui/activities/WellcomeActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.view.View
 import com.example.hsmassistantandroid.R
 import kotlinx.android.synthetic.main.activity_wellcome.*
 
@@ -16,8 +15,10 @@ class WellcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(baseContext)
         val base_url = sharedPreferences.getString("BASE_URL", null)
-        if(base_url != null) {
-            val intent = Intent(baseContext, MainActivity::class.java)
+        val isFirstTime = sharedPreferences.getBoolean("FIRST_TIME", true)
+
+        if(base_url != null || !isFirstTime) {
+            val intent = Intent(baseContext, SetupActivity::class.java)
             startActivity(intent)
             finish()
         }
@@ -26,7 +27,12 @@ class WellcomeActivity : AppCompatActivity() {
         setUpViews()
     }
 
+
     fun setUpViews() {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(baseContext)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("FIRST_TIME", false)
+        editor.apply()
 
         nextButton.setOnClickListener {
             val intent = Intent(baseContext, SetupActivity::class.java)
@@ -34,8 +40,5 @@ class WellcomeActivity : AppCompatActivity() {
             finish()
         }
     }
-
-
-
 }
 
